77e1ca13f15ceeeb30e10742917b94d6cc9f5975
smduarte <smd@fct.unl.pt>
2022-04-03 14:29:11 +0100

    Update FilesRestServer.java

--- src/tp1/impl/service/rest/FilesRestServer.java
+++ src/tp1/impl/service/rest/FilesRestServer.java
@@ -20,12 +20,12 @@ public class FilesRestServer extends AbstractRestServer {
 	void registerResources(ResourceConfig config) {
 		config.register( FilesResources.class ); 
 		config.register( GenericExceptionMapper.class );
-		config.register( CustomLoggingFilter.class);
+//		config.register( CustomLoggingFilter.class);
 	}
 	
 	public static void main(String[] args) throws Exception {
 
-		Log.setLevel( Level.ALL );
+		Log.setLevel( Level.INFO );
 		
 		new FilesRestServer(PORT).start();
 	}	
