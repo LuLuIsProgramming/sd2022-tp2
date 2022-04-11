6822c6fdad781996e04a411b721d6e7b0610b1f2
smduarte <smd@fct.unl.pt>
2022-04-11 10:52:44 +0100

    Update SoapFilesWebService.java

--- src/tp1/impl/service/soap/SoapFilesWebService.java
+++ src/tp1/impl/service/soap/SoapFilesWebService.java
@@ -1,20 +1,24 @@
 package tp1.impl.service.soap;
 
+import java.util.logging.Logger;
+
 import jakarta.jws.WebService;
 import tp1.api.service.java.Files;
 import tp1.api.service.java.Result;
 import tp1.api.service.soap.FilesException;
 import tp1.api.service.soap.SoapFiles;
 import tp1.impl.service.common.JavaFiles;
+import tp1.impl.service.rest.FilesResources;
 
 @WebService(serviceName = SoapFiles.NAME, targetNamespace = SoapFiles.NAMESPACE, endpointInterface = SoapFiles.INTERFACE)
 public class SoapFilesWebService implements SoapFiles {
 
-	private static final String ROOT_DIR = "/tmp/";
+	private static Logger Log = Logger.getLogger(SoapFilesWebService.class.getName());
+
 	final Files impl ;
 	
 	public SoapFilesWebService() {
-		impl = new JavaFiles(ROOT_DIR);
+		impl = new JavaFiles();
 	}
 
 	private <T> T resultOrThrow(Result<T> result) throws FilesException {
@@ -25,22 +29,30 @@ public class SoapFilesWebService implements SoapFiles {
 	}
 
 	@Override
+	public void writeFile(String fileId, byte[] data, String token) throws FilesException {
+		Log.info(String.format("SOAP writeFile: fileId = %s, data.length = %d, token = %s \n", fileId, data.length, token));
+
+		resultOrThrow( impl.writeFile(fileId, data, token));
+	}
+	
+	@Override
+	public void deleteFile(String fileId, String token) throws FilesException {
+		Log.info(String.format("SOAP deleteFile: fileId = %s, token = %s \n", fileId, token));
+
+		resultOrThrow( impl.deleteFile(fileId, token));
+	}
+	
+	@Override
 	public byte[] getFile(String fileId, String token) throws FilesException {
+		Log.info(String.format("SOAP getFile: fileId = %s,  token = %s \n", fileId, token));
+
 		return resultOrThrow( impl.getFile(fileId, token));
 	}
 
 	@Override
-	public void deleteFile(String fileId, String token) throws FilesException {
-		resultOrThrow( impl.deleteFile(fileId, token));
-	}
-
-	@Override
-	public void writeFile(String fileId, byte[] data, String token) throws FilesException {
-		resultOrThrow( impl.writeFile(fileId, data, token));
-	}
-
-	@Override
 	public void deleteUserFiles(String userId, String token) throws FilesException {
+		Log.info(String.format("SOAP deleteUserFiles: userId = %s, token = %s \n", userId, token));
+
 		resultOrThrow( impl.deleteUserFiles(userId, token));
 	}
 }
