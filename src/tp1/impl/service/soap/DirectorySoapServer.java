21bde17714f199c397bf019cfce1640dfeb9845f
smduarte <smd@fct.unl.pt>
2022-04-11 10:51:25 +0100

    Update DirectorySoapServer.java
    fixed name of service used in Discovery announcements

--- src/tp1/impl/service/soap/DirectorySoapServer.java
+++ src/tp1/impl/service/soap/DirectorySoapServer.java
@@ -7,17 +7,19 @@ import java.util.logging.Logger;
 import jakarta.xml.ws.Endpoint;
 import tp1.impl.discovery.Discovery;
 import util.IP;
+import util.Token;
 
 
 public class DirectorySoapServer {
 
 	public static final int PORT = 14567;
-	public static final String SERVICE_NAME = "dir";
+	public static final String SERVICE_NAME = "directory";
 	public static String SERVER_BASE_URI = "http://%s:%s/soap";
 
 	private static Logger Log = Logger.getLogger(DirectorySoapServer.class.getName());
 
 	public static void main(String[] args) throws Exception {
+		Token.set( args[0 ] );
 
 //		System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
 //		System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
