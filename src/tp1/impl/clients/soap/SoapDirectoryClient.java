f3fe0d4d7675d0ab68ced094deeddfa5f9189074
smduarte <smd@fct.unl.pt>
2022-05-08 16:21:59 +0100

    Update SoapDirectoryClient.java

--- src/tp1/impl/clients/soap/SoapDirectoryClient.java
+++ src/tp1/impl/clients/soap/SoapDirectoryClient.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import javax.xml.namespace.QName;
 
-import jakarta.xml.ws.BindingProvider;
 import jakarta.xml.ws.Service;
 import tp1.api.FileInfo;
 import tp1.api.service.java.Directory;
@@ -13,56 +12,48 @@ import tp1.api.service.java.Result;
 import tp1.api.service.soap.SoapDirectory;
 import util.Url;
 
-public class SoapDirectoryClient extends SoapClient implements Directory {
+public class SoapDirectoryClient extends SoapClient<SoapDirectory> implements Directory {
 
 	public SoapDirectoryClient(URI serverURI) {
-		super(serverURI);
-	}
-
-	private SoapDirectory impl;
-
-	synchronized private SoapDirectory impl() {
-		if (impl == null) {
+		super(serverURI, () -> {
 			QName QNAME = new QName(SoapDirectory.NAMESPACE, SoapDirectory.NAME);
-			Service service = Service.create(Url.from(super.uri + WSDL), QNAME);
-			this.impl = service.getPort(tp1.api.service.soap.SoapDirectory.class);
-			super.setTimeouts((BindingProvider) impl);
-		}
-		return impl;
+			Service service = Service.create(Url.from(serverURI + WSDL), QNAME);
+			return service.getPort(tp1.api.service.soap.SoapDirectory.class);			
+		});
 	}
 
 	@Override
 	public Result<FileInfo> writeFile(String filename, byte[] data, String userId, String password) {
-		return super.tryCatchResult(() -> impl().writeFile(filename, data, userId, password));
+		return super.toJavaResult(() -> impl.writeFile(filename, data, userId, password));
 	}
 
 	@Override
 	public Result<Void> deleteFile(String filename, String userId, String password) {
-		return super.tryCatchVoid(() -> impl().deleteFile(filename, userId, password));
+		return super.toJavaResult(() -> impl.deleteFile(filename, userId, password));
 	}
 
 	@Override
 	public Result<Void> shareFile(String filename, String userId, String userIdShare, String password) {
-		return super.tryCatchVoid(() -> impl().deleteFile(filename, userId, password));
+		return super.toJavaResult(() -> impl.deleteFile(filename, userId, password));
 	}
 
 	@Override
 	public Result<Void> unshareFile(String filename, String userId, String userIdShare, String password) {
-		return super.tryCatchVoid(() -> impl().deleteFile(filename, userId, password));
+		return super.toJavaResult(() -> impl.deleteFile(filename, userId, password));
 	}
 
 	@Override
 	public Result<byte[]> getFile(String filename, String userId, String accUserId, String password) {
-		return super.tryCatchResult(() -> impl().getFile(filename, userId, accUserId, password));
+		return super.toJavaResult(() -> impl.getFile(filename, userId, accUserId, password));
 	}
 
 	@Override
 	public Result<List<FileInfo>> lsFile(String userId, String password) {
-		return super.tryCatchResult(() -> impl().lsFile(userId, password));
+		return super.toJavaResult(() -> impl.lsFile(userId, password));
 	}
 
 	@Override
 	public Result<Void> deleteUserFiles(String userId, String password, String token) {
-		return super.tryCatchVoid(() -> impl().deleteUserFiles(userId, password, token));
+		return super.toJavaResult(() -> impl.deleteUserFiles(userId, password, token));
 	}
 }
