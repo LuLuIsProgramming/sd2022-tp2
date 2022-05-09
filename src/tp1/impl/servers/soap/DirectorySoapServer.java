1214b3f82b268e2bb70164ec242c047cf2dce071
smduarte <smd@fct.unl.pt>
2022-05-09 17:39:36 +0100

    Update DirectorySoapServer.java

--- src/tp1/impl/servers/soap/DirectorySoapServer.java
+++ src/tp1/impl/servers/soap/DirectorySoapServer.java
@@ -4,38 +4,26 @@ package tp1.impl.servers.soap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import jakarta.xml.ws.Endpoint;
-import tp1.impl.discovery.Discovery;
-import util.IP;
+import tp1.api.service.java.Directory;
+import util.Debug;
 import util.Token;
 
 
-public class DirectorySoapServer {
+public class DirectorySoapServer extends AbstractSoapServer {
+
 
 	public static final int PORT = 14567;
-	public static final String SERVICE_NAME = "directory";
-	public static String SERVER_BASE_URI = "http://%s:%s/soap";
-
 	private static Logger Log = Logger.getLogger(DirectorySoapServer.class.getName());
 
+	protected DirectorySoapServer() {
+		super(false, Log, Directory.SERVICE_NAME, PORT, new SoapDirectoryWebService());
+	}
+
 	public static void main(String[] args) throws Exception {
-		Token.set( args[0 ] );
 
-//		System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
-//		System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
-//		System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
-//		System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
-
-		Log.setLevel(Level.FINER);
-
-		String ip = IP.hostAddress();
-		String serverURI = String.format(SERVER_BASE_URI, ip, PORT);
-
-		Endpoint.publish(serverURI, new SoapDirectoryWebService());
-
-		Discovery.getInstance().announce(SERVICE_NAME, serverURI);
-
-		Log.info(String.format("%s Soap Server ready @ %s\n", SERVICE_NAME, serverURI));
-
+		Debug.setLogLevel( Level.INFO, Debug.TP1);
+		Token.set( args.length > 0 ? args[0] : "");
+		
+		new DirectorySoapServer().start();
 	}
 }
